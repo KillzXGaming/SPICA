2834ccfe285938f251f05f269d67939cd7b3b6a7
KillzXGaming <millernathan35@gmail.com>
2025-10-12 14:12:42 -0400

    Fix read/writing curves that use constant format

--- SPICA/Formats/CtrGfx/Animation/GfxFloatKeyFrameGroup.cs
+++ SPICA/Formats/CtrGfx/Animation/GfxFloatKeyFrameGroup.cs
@@ -100,13 +100,25 @@ namespace SPICA.Formats.CtrGfx.Animation
                 Curves.Add(curve);
 
                 Deserializer.BaseStream.Seek(pos + i * 4, SeekOrigin.Begin);
+                uint pointer = Deserializer.ReadPointer();
 
-                Deserializer.BaseStream.Seek(Deserializer.ReadPointer(), SeekOrigin.Begin);
+                Deserializer.BaseStream.Seek(pointer, SeekOrigin.Begin);
 
                 curve.StartFrame = Deserializer.Reader.ReadSingle();
                 curve.EndFrame = Deserializer.Reader.ReadSingle();
 
                 uint FormatFlags = Deserializer.Reader.ReadUInt32();
+                if (FormatFlags == 1)
+                {
+                    float constant = Deserializer.Reader.ReadSingle();
+                    curve.Quantization = KeyFrameQuantization.Constant;
+                    curve.KeyFrames.Add(new KeyFrame()
+                    {
+                        Value = constant,
+                    }); 
+                    continue;
+                }
+
                 int KeysCount = Deserializer.Reader.ReadInt32();
                 float InvDuration = Deserializer.Reader.ReadSingle();
 
@@ -248,6 +260,12 @@ namespace SPICA.Formats.CtrGfx.Animation
                 Serializer.Writer.Write(curve.StartFrame);
                 Serializer.Writer.Write(curve.EndFrame);
                 Serializer.Writer.Write(FormatFlags);
+                if (FormatFlags == 1)
+                {
+                    Serializer.Writer.Write(curve.KeyFrames.Count > 0 ? curve.KeyFrames[0].Value : 0.0f);
+                    continue;
+                }
+
                 Serializer.Writer.Write(curve.KeyFrames.Count);
                 Serializer.Writer.Write(InvDuration);
 
